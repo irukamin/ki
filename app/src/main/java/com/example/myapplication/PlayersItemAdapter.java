7c3a2e41a15d1f412fcf58608800aa2f27cfee9a
irukamin <88626826+irukamin@users.noreply.github.com>
2025-03-16 04:57:39 +0900

    更新

--- app/src/main/java/com/example/myapplication/PlayersItemAdapter.java
+++ app/src/main/java/com/example/myapplication/PlayersItemAdapter.java
@@ -51,26 +51,30 @@ public class PlayersItemAdapter extends RecyclerView.Adapter<PlayersItemAdapter.
             // "闇金庫" の場合にのみボタンを表示
             holder.btnAdd.setVisibility(View.VISIBLE);
             holder.btnSubtract.setVisibility(View.VISIBLE);
+            holder.count.setVisibility(View.VISIBLE);
         } else {
             // 他のアイテムにはボタンを非表示
             holder.btnAdd.setVisibility(View.GONE);
             holder.btnSubtract.setVisibility(View.GONE);
+            holder.count.setVisibility(View.GONE);
         }
 
         // プラスボタンとマイナスボタンのクリックリスナーを設定
         holder.btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int currentCount = sharedViewModel.getMoneyCount().getValue();
-                sharedViewModel.setMoneyCount(currentCount + 1);
+                int currentCount = sharedViewModel.getMoneyCount().getValue()+1;
+                sharedViewModel.setMoneyCount(currentCount);
                 sharedViewModel.updateVictoryPoint(itemList);
+                holder.count.setText("金 : "+ currentCount);
             }
         });
 
         holder.btnSubtract.setOnClickListener(v -> {
-            int currentCount = sharedViewModel.getMoneyCount().getValue();
-            if(currentCount>0)sharedViewModel.setMoneyCount(currentCount - 1);
+            int currentCount = sharedViewModel.getMoneyCount().getValue()-1;
+            if(currentCount>0)sharedViewModel.setMoneyCount(currentCount);
             sharedViewModel.updateVictoryPoint(itemList);
+            holder.count.setText("金 : "+currentCount);
         });
     }
 
@@ -84,12 +88,13 @@ public class PlayersItemAdapter extends RecyclerView.Adapter<PlayersItemAdapter.
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
+        TextView textView, count;
         Button btnAdd, btnSubtract;
 
         public ViewHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.itemName);
+            count = itemView.findViewById(R.id.bankcount);
             btnAdd = itemView.findViewById(R.id.btnAdd);
             btnSubtract = itemView.findViewById(R.id.btnSubtract);
             itemView.setOnClickListener(new View.OnClickListener() {
